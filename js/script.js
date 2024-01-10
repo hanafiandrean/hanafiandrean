7b942f722b3e24343b17f2c515f4dfc31745dc1a
hanafianfrean <hanafiandrean25@gmail.com>
2024-01-10 07:02:51 +0700

    finishing web

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// // Toggle class active
+// const navbarNav = document.querySelector('.navbar-nav');
+// //ketika crafting menu di click
+// document.querySelector('#crafting-menu').onclick = () => {
+//   navbarNav.classList.toggle('active');
+// };
+
+// //click diluar side bar untuk ngilangin nav
+// const crafting = document.querySelector('#crafting-menu');
+
+// document.addEventListener('click',function(e){
+//     if(!crafting.contains(e.target) && !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove('active')
+//     }
+// })
+
